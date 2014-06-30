e06ad5cfde18ad9f643d53e55955252f28e8ee56
Andrey Sitnik <andrey@sitnik.ru>
2014-06-30 19:43:10 +0900

    Allow to use ES6 code

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 require('coffee-script').register();
+
+var traceur = require('traceur');
+traceur.require.makeDefault(function(filename) {
+    return filename.indexOf('node_modules') === -1;
+});
+
 module.exports = require('./lib/postcss');
