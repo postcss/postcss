3bc587ba4ed4987d2f422a973ce012ac7cf8d436
Andrey Sitnik <andrey@sitnik.ru>
2014-07-08 14:04:34 +1100

    Compile ES6 by Gulp

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var fs   = require('fs-extra');
+var gulp = require('gulp');
+
+gulp.task('clean', function () {
+    fs.removeSync(__dirname + '/build');
+    fs.removeSync(__dirname + '/fail.css');
+    fs.removeSync(__dirname + '/origin.css');
+});
+
+gulp.task('compile', function () {
+    var traceur = require('gulp-traceur');
+
+    return gulp.src('lib/*.js')
+        .pipe(traceur())
+        .pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('docs', function () {
+    var ignore = fs.readFileSync('.npmignore').toString().trim().split(/\n+/)
+        .concat(['.npmignore', 'package.json', 'index.js'])
+        .map(function (i) { return '!' + i; });
+
+    return gulp.src(['*'].concat(ignore))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('package.json', function () {
+    var editor = require('gulp-json-editor');
+
+    return gulp.src('package.json')
+        .pipe(editor(function (json) {
+            json.main = 'lib/postcss';
+            json.devDependencies.traceur = json.dependencies.traceur
+            delete json.dependencies.traceur;
+            return json;
+        }))
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('build', ['clean', 'compile', 'docs', 'package.json']);
+
