456f247bb2be82e3bbeb0b47c8b8ab2bc3469430
Andrey Sitnik <andrey@sitnik.ru>
2014-07-13 13:08:58 +0400

    Allow to use prebuild from git

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var traceur = require('traceur');
-traceur.require.makeDefault(function(filename) {
-    return filename.indexOf('node_modules') === -1;
+
+traceur.require.makeDefault(function(file) {
+    return file.indexOf(__dirname + '/lib')  != -1 ||
+           file.indexOf(__dirname + '/test') != -1 ;
 });
 
 module.exports = require('./lib/postcss');
