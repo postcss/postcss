0306a7b2829ccda25f42009f3cc0019bc0b19ecf
Andrey Sitnik <andrey@sitnik.ru>
2014-08-18 15:03:57 +0400

    Better should.js loading

--- gulpfile.js
+++ gulpfile.js
@@ -247,6 +247,8 @@ gulp.task('integration', ['build'], function (done) {
 
 gulp.task('test', function () {
     require('./');
+    require('should');
+
     var mocha = require('gulp-mocha');
     return gulp.src('test/*.js', { read: false }).pipe(mocha());
 });
