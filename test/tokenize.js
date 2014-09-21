ec162703e98a35dc8e7ed8f533beecf74533ddb0
Andrey Sitnik <andrey@sitnik.ru>
2014-09-21 23:13:17 +0400

    Add brackets token

--- test/tokenize.js
+++ test/tokenize.js
@@ -18,32 +18,39 @@ describe('tokenize', () => {
     it('tokenizes CSS', () => {
         css = 'a {\n' +
               '  content: "a";\n' +
+              '  width: calc(1px;)\n' +
               '  }\n' +
               '/* small screen */\n' +
               '@media screen {}';
         tokenize(css).should.eql([
-            ['word', 'a', { column: 1, line: 1 }, { column: 2, line: 1 } ],
+            ['word', 'a', { column: 1, line: 1 }, { column: 1, line: 1 } ],
             ['space', ' '],
             ['{', '{'],
             ['space', '\n  '],
-            ['word', 'content', { column: 3, line: 2 },
-                                { column: 10, line: 2 }],
+            ['word', 'content', { column: 3, line: 2 }, { column: 9, line: 2 }],
             [':', ':'],
             ['space', ' '],
-            ['string', '"a"'],
-            [';', ';'],
+            ['string', '"a"', { column: 12, line: 2 }, { column: 14, line: 2 }],
+            [';', ';', { column: 15, line: 2 }],
             ['space', '\n  '],
-            ['}', '}', { column: 3, line: 3 }],
-            ['space', '\n'],
-            ['comment', '/* small screen */', { column: 1, line: 4 },
-                                              { column: 18, line: 4 }],
-            ['space', '\n'],
-            ['at-word', '@media', { column: 1, line: 5 }],
+            ['word', 'width', { column: 3, line: 3 }, { column: 7, line: 3 }],
+            [':', ':'],
             ['space', ' '],
-            ['word', 'screen', { column: 8, line: 5 }, { column: 14, line: 5 }],
+            ['word', 'calc', { column: 10, line: 3 }, { column: 13, line: 3 }],
+            ['brackets', '(1px;)', { column: 14, line: 3 },
+                                   { column: 19, line: 3 }],
+            ['space', '\n  '],
+            ['}', '}', { column: 3, line: 4 }],
+            ['space', '\n'],
+            ['comment', '/* small screen */', { column: 1, line: 5 },
+                                              { column: 18, line: 5 }],
+            ['space', '\n'],
+            ['at-word', '@media', { column: 1, line: 6 }],
+            ['space', ' '],
+            ['word', 'screen', { column: 8, line: 6 }, { column: 13, line: 6 }],
             ['space', ' '],
             ['{', '{'],
-            ['}', '}', { column: 16, line: 5 }]
+            ['}', '}', { column: 16, line: 6 }]
         ]);
     });
 
@@ -56,7 +63,9 @@ describe('tokenize', () => {
     });
 
     it('fixes unclosed string in safe smode', () => {
-        tokenize('"', { safe: true }).should.eql([ ['string', '""'] ]);
+        tokenize('"', { safe: true }).should.eql([
+            ['string', '""', { column: 1, line: 1 }, { column: 1, line: 1 }]
+        ]);
     });
 
     it('throws error on unclosed comment', () => {
