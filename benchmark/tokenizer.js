5498637c1b27c20109a4300e4bc47594795119c4
Termina1 <terminal2010@gmail.com>
2014-10-02 23:46:29 +0400

    speed improvements

--- benchmark/tokenizer.js
+++ benchmark/tokenizer.js
@@ -1,6 +1,6 @@
 fs = require('fs');
-var style = fs.readFileSync(__dirname + '/fixtures/test.css').toString();
-var tokenizer = require('build/lib/tokenize');
+style = fs.readFileSync(__dirname + '/fixtures/test.css').toString();
+tokenizer = require('build/lib/tokenize');
 module.exports = {
   fn: function() {
     tokenizer(style);
