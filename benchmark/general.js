d29ca49a6f0c7ecba0c1cb3ca6acdc748a6fc66e
Andrey Sitnik <andrey@sitnik.ru>
2014-10-04 21:06:23 +0400

    Clean up development tasks

---
+++ benchmark/general.js
@@ -0,0 +1,47 @@
+var fs  = require('fs');
+var css = fs.readFileSync(__dirname + '/cache/bootstrap.css').toString();
+
+var CSSOM      = require('cssom');
+var postcss    = require('../build');
+var rework     = require('rework');
+var gonzales   = require('gonzales');
+var gonzalesPe = require('gonzales-pe');
+
+module.exports = {
+    name:   'Bootstrap',
+    maxTime: 15,
+    tests: [
+        {
+            name: 'PostCSS',
+            fn: function() {
+                return postcss.parse(css).toResult().css;
+            }
+        },
+        {
+            name: 'CSSOM',
+            fn: function() {
+                return CSSOM.parse(css).toString();
+            }
+        },
+        {
+            name: 'Rework',
+            fn: function() {
+                return rework(css).toString();
+            }
+        },
+        {
+            name: 'Gonzales',
+            fn: function() {
+                return gonzales.csspToSrc( gonzales.srcToCSSP(css) );
+            }
+        },
+        {
+            name: 'Gonzales PE',
+            fn: function() {
+                return gonzalesPe.astToSrc({
+                    ast: gonzalesPe.srcToAST({ src: css })
+                });
+            }
+        },
+    ]
+};
