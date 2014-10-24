d0c44da4a37372dbe7f3db70f83d28e0f1a9d40c
Andrey Sitnik <andrey@sitnik.ru>
2014-10-24 18:40:17 +0400

    Safer 6to5 register

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-require('6to5/register')(/node_modules|build|benchmark|tasks|gulpfile.js/);
+var path = require('path');
+
+var root   = __dirname.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+var ignore = ['node_modules', 'tasks', 'build', 'benchmark', 'gulpfile.js'];
+var escape = function (str) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+};
+
+var regexp = ignore.map(function (i) {
+    return escape(path.join(__dirname, i));
+}).join('|');
+
+require('6to5/register')(new RegExp(regexp));
 module.exports = require('./lib/postcss');
