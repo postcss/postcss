9e593f6644de75515c60ee26c7df89c6460e202a
Andrey Sitnik <andrey@sitnik.ru>
2014-11-07 05:28:06 +0300

    Use better ES6 hook from 6to5 1.10.11

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var path = require('path');
 
 var root   = __dirname.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-var ignore = ['node_modules', 'tasks', 'build', 'benchmark', 'gulpfile.js'];
 var escape = function (str) {
     return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 };
 
-var regexp = ignore.map(function (i) {
+var regexp = ['lib', 'test'].map(function (i) {
     return escape(path.join(__dirname, i));
 }).join('|');
 
-require('6to5/register')(new RegExp(regexp));
+require('6to5/register')({ only: new RegExp(regexp) });
 module.exports = require('./lib/postcss');
