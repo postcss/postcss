753fef077420cc5db583033d8c7586cff2d7cc57
Andrey Sitnik <andrey@sitnik.ru>
2014-11-10 16:15:31 +0300

    Fix tokenizer error handling

--- test/tokenize.js
+++ test/tokenize.js
@@ -74,6 +74,19 @@ describe('tokenize', () => {
         ]);
     });
 
+    it('tokenizes at-word', () => {
+        tokenize(new Input('@word ')).should.eql([
+            ['at-word', '@word', 1, 1, 1, 5],
+            ['space',   ' ']
+        ]);
+    });
+
+    it('tokenizes at-symbol', () => {
+        tokenize(new Input('@')).should.eql([
+            ['at-word', '@', 1, 1, 1, 1]
+        ]);
+    });
+
     it('tokenizes comment', () => {
         tokenize(new Input('/* a\nb */')).should.eql([
             ['comment', '/* a\nb */', 1, 1, 2, 4]
@@ -96,43 +109,54 @@ describe('tokenize', () => {
               '/* small screen */\n' +
               '@media screen {}';
         tokenize(new Input(css)).should.eql([
-            ['word', 'a', 1, 1, 1, 1],
-            ['space', ' '],
-            ['{', '{', 1, 3],
-            ['space', '\n  '],
-            ['word', 'content', 2, 3, 2, 9],
-            [':', ':', 2, 10],
-            ['space', ' '],
-            ['string', '"a"', 2, 12, 2, 14],
-            [';', ';', 2, 15],
-            ['space', '\n  '],
-            ['word', 'width', 3, 3, 3, 7],
-            [':', ':', 3, 8],
-            ['space', ' '],
-            ['word', 'calc', 3, 10, 3, 13],
-            ['brackets', '(1px;)', 3, 14, 3, 19],
-            ['space', '\n  '],
-            ['}', '}', 4, 3],
-            ['space', '\n'],
-            ['comment', '/* small screen */', 5, 1, 5, 18],
-            ['space', '\n'],
-            ['at-word', '@media', 6, 1, 6, 6],
-            ['space', ' '],
-            ['word', 'screen', 6, 8, 6, 13],
-            ['space', ' '],
-            ['{', '{', 6, 15],
-            ['}', '}', 6, 16]
+            ['word',     'a',                  1,  1, 1,  1],
+            ['space',    ' '],
+            ['{',        '{',                  1,  3],
+            ['space',    '\n  '],
+            ['word',     'content',            2,  3, 2,  9],
+            [':',        ':',                  2, 10],
+            ['space',    ' '],
+            ['string',   '"a"',                2, 12, 2, 14],
+            [';',        ';',                  2, 15],
+            ['space',    '\n  '],
+            ['word',     'width',              3,  3, 3,  7],
+            [':',        ':',                  3,  8],
+            ['space',    ' '],
+            ['word',     'calc',               3, 10, 3, 13],
+            ['brackets', '(1px;)',             3, 14, 3, 19],
+            ['space',    '\n  '],
+            ['}',        '}',                  4,  3],
+            ['space',    '\n'],
+            ['comment',  '/* small screen */', 5,  1, 5, 18],
+            ['space',    '\n'],
+            ['at-word',  '@media',             6,  1, 6,  6],
+            ['space',    ' '],
+            ['word',     'screen',             6,  8, 6, 13],
+            ['space',    ' '],
+            ['{',        '{',                  6, 15],
+            ['}',        '}',                  6, 16]
         ]);
     });
 
     it('throws error on unclosed string', () => {
         ( () => tokenize(new Input(' "')) )
-            .should.throw(/:1:2: Unclosed quote/);
+           .should.throw(/:1:2: Unclosed quote/);
     });
 
-    it('fixes unclosed string in safe smode', () => {
+    it('fixes unclosed string in safe mode', () => {
         tokenize(new Input('"', { safe: true })).should.eql([
-            ['string', '""', 1, 1, 1, 1]
+            ['string', '""', 1, 1, 1, 2]
+        ]);
+    });
+
+    it('throws error on unclosed bracket', () => {
+        ( () => tokenize(new Input(' (')) )
+           .should.throw(/:1:2: Unclosed bracket/);
+    });
+
+    it('fixes unclosed bracket in safe mode', () => {
+        tokenize(new Input('(', { safe: true })).should.eql([
+            ['brackets', '()', 1, 1, 1, 2]
         ]);
     });
 
@@ -141,9 +165,9 @@ describe('tokenize', () => {
             .should.throw(/:1:2: Unclosed comment/);
     });
 
-    it('fixes unclosed comment in safe smode', () => {
+    it('fixes unclosed comment in safe mode', () => {
         tokenize(new Input('/*', { safe: true })).should.eql([
-            ['comment', '/**/', 1, 1, 1, 2]
+            ['comment', '/**/', 1, 1, 1, 4]
         ]);
     });
 
