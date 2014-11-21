ec1aee5460f0fe445578eba00cab3c6937ba245d
Andrey Sitnik <andrey@sitnik.ru>
2014-11-21 15:51:13 +0300

    Update dependencies

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ var regexp = ['lib', 'test'].map(function (i) {
 
 require('6to5/register')({
     only:   new RegExp('(' + regexp + ')'),
-    ignore: false
+    ignore: null
 });
 module.exports = require('./lib/postcss');
