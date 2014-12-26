c986217f4bcb167c0a24c48044a9afa6a4560539
Andrey Sitnik <andrey@sitnik.ru>
2014-12-26 23:46:40 +0300

    Add Node#moveBefore() and moveAfter()

--- test/node.js
+++ test/node.js
@@ -193,6 +193,48 @@ describe('Node', () => {
 
     });
 
+    describe('moveBefore()', () => {
+
+        it('moves node between roots', () => {
+            var css1 = parse('a{one:1}b{two:2}');
+            var css2 = parse('c {\n  thr: 3\n}');
+            css1.first.moveBefore(css2.first);
+
+            expect(css1.toString()).to.eql('b{two:2}');
+            expect(css2.toString()).to.eql('a {\n  one: 1\n}c {\n  thr: 3\n}');
+        });
+
+        it('moves node inside one root', () => {
+            var css = parse('a{\n one:1}\n@page {\n b {\n  two: 2\n }\n}');
+            css.first.moveBefore(css.last.first);
+
+            expect(css.toString())
+                .to.eql('@page {\n a{\n  one:1\n }\n b {\n  two: 2\n }\n}');
+        });
+
+    });
+
+    describe('moveAfter()', () => {
+
+        it('moves node between roots', () => {
+            var css1 = parse('a{one:1}b{two:2}');
+            var css2 = parse('c {\n  thr: 3\n}');
+            css1.first.moveAfter(css2.first);
+
+            expect(css1.toString()).to.eql('b{two:2}');
+            expect(css2.toString()).to.eql('c {\n  thr: 3\n}a {\n  one: 1\n}');
+        });
+
+        it('moves node inside one root', () => {
+            var css = parse('a{\n one:1}\n@page {\n b {\n  two: 2\n }\n}');
+            css.first.moveAfter(css.last.first);
+
+            expect(css.toString())
+                .to.eql('@page {\n b {\n  two: 2\n }\n a{\n  one:1\n }\n}');
+        });
+
+    });
+
     describe('toJSON()', () => {
 
         it('cleans parents inside', () => {
