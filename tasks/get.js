e98b200b13981a0e2e9e0140c7cc9a47c9abc2bc
Andrey Sitnik <andrey@sitnik.ru>
2015-04-28 01:03:37 +0300

    Try again after network problem in integration test

--- tasks/get.js
+++ tasks/get.js
@@ -1,37 +1,48 @@
 var request = require('request');
 var zlib    = require('zlib');
 
+var load = function (url, callback, errors) {
+    request.get({
+        url:     url,
+        headers: { 'accept-encoding': 'gzip,deflate' }
+    }).on('error', function (error) {
+        if ( errors > 2 ) throw error;
+        console.error(error.toString());
+        load(url, callback, (errors || 1) + 1);
+    }).on('response', function (res) {
+        callback(res);
+    });
+};
+
 module.exports = function (url, callback) {
     if ( url.match(/^github:/) ) {
-        var parts = url.split(':');
-        url = 'https://raw.githubusercontent.com/' +
-            parts[1] + '/master/' + parts[2];
+        var p = url.split(':');
+        url = 'https://raw.githubusercontent.com/' + p[1] + '/master/' + p[2];
     }
 
-    request.get({ url: url, headers: {'accept-encoding': 'gzip,deflate'} })
-        .on('response', function (res) {
-            var chunks = [];
-            res.on('data', function (i) {
-                chunks.push(i);
-            });
-            res.on('end', function () {
-                var buffer = Buffer.concat(chunks);
-
-                if ( res.headers['content-encoding'] === 'gzip' ) {
-                    zlib.gunzip(buffer, function (err, decoded) {
-                        if ( err ) throw err;
-                        callback(decoded.toString());
-                    });
-
-                } else if ( res.headers['content-encoding'] === 'deflate' ) {
-                    zlib.inflate(buffer, function (err, decoded) {
-                        if ( err ) throw err;
-                        callback(decoded.toString());
-                    });
-
-                } else {
-                    callback(buffer.toString());
-                }
-            });
+    load(url, function (res) {
+        var chunks = [];
+        res.on('data', function (i) {
+            chunks.push(i);
         });
+        res.on('end', function () {
+            var buffer = Buffer.concat(chunks);
+
+            if ( res.headers['content-encoding'] === 'gzip' ) {
+                zlib.gunzip(buffer, function (err, decoded) {
+                    if ( err ) throw err;
+                    callback(decoded.toString());
+                });
+
+            } else if ( res.headers['content-encoding'] === 'deflate' ) {
+                zlib.inflate(buffer, function (err, decoded) {
+                    if ( err ) throw err;
+                    callback(decoded.toString());
+                });
+
+            } else {
+                callback(buffer.toString());
+            }
+        });
+    });
 };
