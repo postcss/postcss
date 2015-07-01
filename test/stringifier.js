01a920e2415d6fddecf1d2abbb246a4ee79220a9
Andrey Sitnik <andrey@sitnik.ru>
2015-07-01 16:31:05 +0300

    Move Node#style to Stringifier

--- test/stringifier.js
+++ test/stringifier.js
@@ -1,12 +1,20 @@
 import Stringifier from '../lib/stringifier';
-import Node        from '../lib/node';
+import Declaration from '../lib/declaration';
+import AtRule      from '../lib/at-rule';
 import parse       from '../lib/parse';
+import Node        from '../lib/node';
+import Root        from '../lib/root';
+import Rule        from '../lib/rule';
 
 import { expect } from 'chai';
 import   path     from 'path';
 import   fs       from 'fs';
 
 describe('stringifier', () => {
+    let str;
+    before( () => {
+        str = new Stringifier();
+    });
 
     describe('.stringify()', () => {
 
@@ -24,10 +32,6 @@ describe('stringifier', () => {
     });
 
     describe('.raw()', () => {
-        let str;
-        before( () => {
-            str = new Stringifier();
-        });
 
         it('creates trimmed/raw property', () => {
             let b = new Node({ one: 'trim' });
@@ -44,6 +48,123 @@ describe('stringifier', () => {
             expect(b.one).to.eql('1');
             expect(str.raw(b, 'one')).to.eql('1');
         });
+    });
+
+    describe('style()', () => {
+
+        it('uses node style', () => {
+            let rule = new Rule({ selector: 'a', raw: { between: '\n' } });
+            expect(str.style(rule, 'between', 'beforeOpen')).to.eql('\n');
+        });
+
+        it('hacks before for nodes without parent', () => {
+            let rule = new Rule({ selector: 'a' });
+            expect(str.style(rule, 'before')).to.eql('');
+        });
+
+        it('hacks before for first node', () => {
+            let root = new Root();
+            root.append(new Rule({ selector: 'a' }));
+            expect(str.style(root.first, 'before')).to.eql('');
+        });
+
+        it('hacks before for first decl', () => {
+            let decl = new Declaration({ prop: 'color', value: 'black' });
+            expect(str.style(decl, 'before')).to.eql('');
+
+            let rule = new Rule({ selector: 'a' });
+            rule.append(decl);
+            expect(str.style(decl, 'before')).to.eql('\n    ');
+        });
+
+        it('detects after style', () => {
+            let root = new Root();
+            root.append({ selector: 'a', raw: { after: ' ' } });
+            root.first.append({ prop: 'color', value: 'black' });
+            root.append({ selector: 'a' });
+            expect(str.style(root.last, 'after')).to.eql(' ');
+        });
+
+        it('uses defaults without parent', () => {
+            let rule = new Rule({ selector: 'a' });
+            expect(str.style(rule, 'between', 'beforeOpen')).to.eql(' ');
+        });
+
+        it('uses defaults for unique node', () => {
+            let root = new Root();
+            root.append(new Rule({ selector: 'a' }));
+            expect(str.style(root.first, 'between', 'beforeOpen')).to.eql(' ');
+        });
+
+        it('clones style from first node', () => {
+            let root = new Root();
+            root.append( new Rule({ selector: 'a', raw: { between: '' } }) );
+            root.append( new Rule({ selector: 'b' }) );
+
+            expect(str.style(root.last, 'between', 'beforeOpen')).to.eql('');
+        });
+
+        it('indents by default', () => {
+            let root = new Root();
+            root.append( new AtRule({ name: 'page' }) );
+            root.first.append( new Rule({ selector: 'a' }) );
+            root.first.first.append({ prop: 'color', value: 'black' });
+
+            expect(root.toString()).to.eql('@page {\n' +
+                                           '    a {\n' +
+                                           '        color: black\n' +
+                                           '    }\n' +
+                                           '}');
+        });
+
+        it('clones indent', () => {
+            let compress = parse('@page{ a{ } }');
+            let spaces   = parse('@page {\n  a {\n  }\n}');
+
+            compress.first.first.append({ prop: 'color', value: 'black' });
+            expect(compress.toString()).to.eql('@page{ a{ color: black } }');
+
+            spaces.first.first.append({ prop: 'color', value: 'black' });
+            expect(spaces.toString())
+                .to.eql('@page {\n  a {\n    color: black\n  }\n}');
+        });
+
+        it('clones indent by types', () => {
+            let css = parse('a {\n  color: black\n}\n\nb {\n}');
+            css.append(new Rule({ selector: 'em' }));
+            css.last.append({ prop: 'z-index', value: '1' });
+
+            expect(css.last.toString()).to.eql('\n\nem {\n  z-index: 1\n}');
+        });
+
+        it('clones indent by before and after', () => {
+            let css = parse('@page{\n\n a{\n  color: black}}');
+            css.first.append(new Rule({ selector: 'b' }));
+            css.first.last.append({ prop: 'z-index', value: '1' });
+
+            expect(css.first.last.toString()).to.eql('\n\n b{\n  z-index: 1}');
+        });
+
+        it('clones semicolon only from rules with children', () => {
+            let css = parse('a{}b{one:1;}');
+            expect(str.style(css.first, 'semicolon')).to.be.true;
+        });
+
+        it('clones only spaces in before', () => {
+            let css = parse('a{*one:1}');
+            css.first.append({ prop: 'two', value: '2' });
+            css.append({ name: 'keyframes', params: 'a' });
+            css.last.append({ selector: 'from'});
+            expect(css.toString())
+                .to.eql('a{*one:1;two:2}\n@keyframes a{\nfrom{}}');
+        });
+
+        it('clones only spaces in between', () => {
+            let css = parse('a{one/**/:1}');
+            css.first.append({ prop: 'two', value: '2' });
+            expect(css.toString()).to.eql('a{one/**/:1;two:2}');
+        });
+
     });
 
 });
