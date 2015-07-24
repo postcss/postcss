0a0e05e0bc49e7908d1c1bc98338148fe449b501
Jed Mao <jedmao@outlook.com>
2015-07-24 15:55:15 -0500

    Introduce TypeScript definitions

---
+++ d.ts/at-rule.d.ts
@@ -0,0 +1,41 @@
+import Container from './container';
+import postcss from './postcss';
+export default class AtRule extends Container implements postcss.AtRule {
+    /**
+     * Returns a string representing the node's type. Possible values are
+     * root, atrule, rule, decl or comment.
+     */
+    type: string;
+    /**
+     * Contains information to generate byte-to-byte equal node string as it
+     * was in origin input.
+     */
+    raws: postcss.AtRuleRaws;
+    /**
+     * The identifier that immediately follows the @.
+     */
+    name: string;
+    /**
+     * These are the values that follow the at-rule's name, but precede any {}
+     * block. The spec refers to this area as the at-rule's "prelude".
+     */
+    params: string;
+    /**
+     * Represents an at-rule. This node will have a nodes property,
+     * representing its children, if it is followed in the CSS by a {} block.
+     */
+    constructor(defaults?: postcss.AtRuleNewProps);
+    /**
+     * @param overrides New properties to override in the clone.
+     * @returns A clone of this node. The node and its (cloned) children will
+     * have a clean parent and code style properties.
+     */
+    clone(overrides?: Object): AtRule;
+    toJSON(): postcss.JsonAtRule;
+    append(...children: any[]): Container;
+    prepend(...children: any[]): Container;
+    insertBefore(oldNode: any, newNode: any): Container;
+    insertAfter(oldNode: any, newNode: any): Container;
+    afterName: string;
+    _params: string;
+}
---
+++ d.ts/css-syntax-error.d.ts
@@ -0,0 +1,120 @@
+import postcss from './postcss';
+export default class CssSyntaxError implements postcss.CssSyntaxError, SyntaxError {
+    /**
+     * Contains full error text by GNU error format.
+     */
+    message: string;
+    /**
+     * Contains source line of error. PostCSS will use the input source map
+     * to detect the original error location. If you wrote a Sass file, then
+     * compiled it to CSS and parsed it with PostCSS, PostCSS will show the
+     * original position in the Sass file. If you need position in PostCSS
+     * input (e.g., to debug previous compiler), use error.generated.line.
+     */
+    line: number;
+    /**
+     * Contains the source column of the error. PostCSS will use input
+     * source map to detect the original error location. If you wrote a Sass
+     * file, then compiled it to CSS and parsed it with PostCSS, PostCSS
+     * will show the original position in the Sass file. If you need
+     * position in PostCSS input (e.g., to debug previous compiler), use
+     * error.generated.column.
+     */
+    column: number;
+    /**
+     * Contains source code of broken file. PostCSS will use input source
+     * map to detect the original error location. If you wrote a Sass file,
+     * then compiled it to CSS and parsed it with PostCSS, PostCSS will show
+     * the original position in the Sass file. If you need position in
+     * PostCSS input (e.g., to debug previous compiler), use
+     * error.generated.source.
+     */
+    source: string;
+    /**
+     * If parser's from option is set, contains absolute path to broken file.
+     * PostCSS will use the input source map to detect the original error
+     * location. If you wrote a Sass file, then compiled it to CSS and
+     * parsed it with PostCSS, PostCSS will show the original position in
+     * the Sass file. If you need the position in PostCSS input
+     * (e.g., to debug previous compiler), use error.generated.file.
+     */
+    file: string;
+    /**
+     * Contains PostCSS plugin name if error did not come from CSS parser.
+     */
+    plugin: string;
+    name: string;
+    /**
+     * Contains only the error description.
+     */
+    reason: string;
+    private columnNumber;
+    private description;
+    private lineNumber;
+    private fileName;
+    input: postcss.InputOrigin;
+    /**
+     * The CSS parser throws this error for broken CSS.
+     */
+    constructor(
+        /**
+         * Contains full error text by GNU error format.
+         */
+        message: string, 
+        /**
+         * Contains source line of error. PostCSS will use the input source map
+         * to detect the original error location. If you wrote a Sass file, then
+         * compiled it to CSS and parsed it with PostCSS, PostCSS will show the
+         * original position in the Sass file. If you need position in PostCSS
+         * input (e.g., to debug previous compiler), use error.generated.line.
+         */
+        line?: number, 
+        /**
+         * Contains the source column of the error. PostCSS will use input
+         * source map to detect the original error location. If you wrote a Sass
+         * file, then compiled it to CSS and parsed it with PostCSS, PostCSS
+         * will show the original position in the Sass file. If you need
+         * position in PostCSS input (e.g., to debug previous compiler), use
+         * error.generated.column.
+         */
+        column?: number, 
+        /**
+         * Contains source code of broken file. PostCSS will use input source
+         * map to detect the original error location. If you wrote a Sass file,
+         * then compiled it to CSS and parsed it with PostCSS, PostCSS will show
+         * the original position in the Sass file. If you need position in
+         * PostCSS input (e.g., to debug previous compiler), use
+         * error.generated.source.
+         */
+        source?: string, 
+        /**
+         * If parser's from option is set, contains absolute path to broken file.
+         * PostCSS will use the input source map to detect the original error
+         * location. If you wrote a Sass file, then compiled it to CSS and
+         * parsed it with PostCSS, PostCSS will show the original position in
+         * the Sass file. If you need the position in PostCSS input
+         * (e.g., to debug previous compiler), use error.generated.file.
+         */
+        file?: string, 
+        /**
+         * Contains PostCSS plugin name if error did not come from CSS parser.
+         */
+        plugin?: string);
+    private setMessage();
+    /**
+     * @param color Whether arrow should be colored red by terminal color codes.
+     * By default, PostCSS will use process.stdout.isTTY and
+     * process.env.NODE_DISABLE_COLORS.
+     * @returns A few lines of CSS source that caused the error. If CSS has
+     * input source map without sourceContent this method will return an empty
+     * string.
+     */
+    showSourceCode(color?: boolean): string;
+    private highlight(color);
+    private setMozillaProps();
+    /**
+     *
+     * @returns Error position, message and source code of broken part.
+     */
+    toString(): string;
+}
