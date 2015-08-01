a7bc456ae9b9d3084541578cb9a18085329482c3
Andrey Sitnik <andrey@sitnik.ru>
2015-08-02 02:17:17 +0300

    Fix parse tests on Windows

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -130,12 +130,13 @@ gulp.task('test', () => {
 
 gulp.task('cases', () => {
     let postcss = require('./lib/postcss');
+    let jsonify = require('./test/utils/jsonify');
     let cases   = path.join(__dirname, 'test', 'cases');
     fs.readdirSync(cases).forEach( (name) => {
         if ( !name.match(/\.css$/) ) return;
         let css  = fs.readFileSync(path.join(cases, name));
         let root = postcss.parse(css, { from: '/' + name });
-        let json = JSON.stringify(root, null, 4);
+        let json = JSON.stringify(jsonify(root), null, 4);
         let file = path.join(cases, name.replace(/\.css$/, '.json'));
         fs.writeFileSync(file, json + '\n');
     });
