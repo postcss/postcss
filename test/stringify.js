46a514bc3c5b2a607e918505766a30fd43f21f32
Andrey Sitnik <andrey@sitnik.ru>
2015-08-02 21:58:57 +0300

    Move cases tests to postcss-parser-tests

--- test/stringify.js
+++ test/stringify.js
@@ -2,22 +2,17 @@ import stringify from '../lib/stringify';
 import parse     from '../lib/parse';
 
 import { expect } from 'chai';
+import   cases    from 'postcss-parser-tests';
 import   path     from 'path';
 import   fs       from 'fs';
 
 describe('stringify', () => {
 
-    fs.readdirSync(path.join(__dirname, 'cases')).forEach( name => {
-        if ( !name.match(/\.css$/) ) return;
-
+    cases.each( (name, css, json) => {
         it('stringifies ' + name, () => {
-            let file = path.join(__dirname, 'cases', name);
-            let css  = fs.readFileSync(file).toString();
-            let root = parse(css, { from: file });
-
+            let root = parse(css, { from: name });
             let result = '';
             stringify(root, i => result += i );
-
             expect(result).to.eql(css);
         });
     });
