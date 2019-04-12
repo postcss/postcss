e2e588b5095e767547b1c4eaf983a85b844ebf9f
Andrey Sitnik <andrey@sitnik.ru>
2019-04-12 13:50:03 -0400

    Remove Babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,21 @@ let gulp = require('gulp')
 
 gulp.task('clean', () => {
   let del = require('del')
-  return del(['lib/*.js', 'postcss.js', 'build/', 'api/'])
+  return del(['build/', 'api/'])
 })
 
 // Build
 
-gulp.task('compile', () => {
-  let sourcemaps = require('gulp-sourcemaps')
-  let changed = require('gulp-changed')
-  let babel = require('gulp-babel')
-  return gulp.src('lib/*.es6')
-    .pipe(changed('lib', { extension: '.js' }))
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('lib'))
-})
-
-gulp.task('build:lib', gulp.series('compile', () => {
+gulp.task('build:lib', () => {
   return gulp.src(['lib/*.js', 'lib/*.d.ts']).pipe(gulp.dest('build/lib'))
-}))
+})
 
 gulp.task('build:package', () => {
   let editor = require('gulp-json-editor')
   return gulp.src('./package.json')
     .pipe(editor(json => {
-      delete json.babel
       delete json.scripts
       delete json.jest
-      delete json.babel
       delete json.eslintConfig
       delete json['size-limit']
       delete json['pre-commit']
