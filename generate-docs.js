48cc08a922197dae98b5c42b797128d62cc5e0f4
Ivan Solovev <ivan@solovev.one>
2019-05-06 21:44:13 +0300

    Change bundling documentation

--- generate-docs.js
+++ generate-docs.js
@@ -1,57 +1,32 @@
 let fs = require('fs')
-let { join } = require('path')
-let { Readable } = require('stream')
+let path = require('path')
+let { promisify } = require('util')
 let documentation = require('documentation')
-let vfs = require('vinyl-fs')
 
-let API_FOLDER = join(process.cwd(), 'api')
+let mkdir = promisify(fs.mkdir)
+let writeFile = promisify(fs.writeFile)
 
-let config = {
-  extension: 'js',
-  shallow: true,
-  github: true
-}
+let API_FOLDER = path.join(process.cwd(), 'api')
 
 documentation
-  .build('./lib/*.js', config)
+  .build('./lib/*.js', {
+    shallow: true,
+    github: true
+  })
   .then(generateDocs)
   .then(renderTemplate)
   .then(saveDocs)
-  .catch(renderError)
-
-function renderTemplate (output) {
-  return documentation.formats.html(output, {
-    theme: './node_modules/documentation-theme-light'
+  .catch(error => {
+    process.stderr.write(error.stack + '\n')
+    process.exit(1)
   })
-}
 
 /**
- * Save documentation
- * @param {Array} output
- * @return {*}
- */
-function saveDocs (output) {
-  return new StreamArray(output)
-    .pipe(vfs.dest(API_FOLDER))
-    .on('end', addMarginForNote)
-}
-
-/**
- * Render error
- * @param {Error} error
- */
-function renderError (error) {
-  process.stderr.write(error.stack + '\n')
-  process.exit(1)
-}
-
-/**
- * Generate documentation with extended classes
  * @param {Object} data Entrance data
  * @return {Array}
  */
 function generateDocs (data) {
-  let docs = objectValues(data)
+  let docs = Object.values(data)
 
   docs
     .filter(item => item.augments.length > 0)
@@ -66,31 +41,26 @@ function generateDocs (data) {
 
           doc.members.instance = []
             .concat(doc.members.instance, instanceEssence)
-            .map(item => Object.assign(item, {
+            .map(item => ({
+              ...item,
               namespace: item.namespace.replace(essence.name, doc.name)
             }))
-            .filter((item, index, list) =>
-              item.memberof === essence.name ||
-              (
-                item.memberof === doc.name &&
-                !list.find(e => e.name === item.name &&
-                  e.memberof === essence.name
+            .filter((item, index, list) => {
+              return item.memberof === essence.name ||
+                (
+                  item.memberof === doc.name &&
+                  !list.find(e => e.name === item.name &&
+                    e.memberof === essence.name
+                  )
                 )
-              )
-            )
+            })
             .sort((a, b) => a.name > b.name ? 1 : -1)
         }
       })
     })
 
   docs
-    .sort((a, b) => {
-      if (a.kind === b.kind) {
-        return a.name > b.name ? 1 : -1
-      }
-
-      return a.kind > b.kind ? 1 : -1
-    })
+    .sort((a, b) => a.name > b.name ? 1 : -1)
     .forEach(doc => {
       doc.members.instance = doc.members.instance
         .filter(item => item.name)
@@ -112,33 +82,43 @@ function generateDocs (data) {
   )
 }
 
-/**
- * Adding indentation to sections
- */
-function addMarginForNote () {
-  let indexFile = join(API_FOLDER, 'index.html')
-
-  fs.readFile(indexFile, { encoding: 'utf-8' }, (error, data) => {
-    if (error) {
-      renderError(error)
-    }
-
-    let html = data
-      .replace(
-        /regular blockmt1 quiet rounded/g,
-        'blockmt1 quiet rounded bold block h4 mt2'
-      )
-
-    fs.writeFile(indexFile, html, errorWrite => {
-      if (errorWrite) {
-        renderError(errorWrite)
-      }
-    })
+function renderTemplate (output) {
+  return documentation.formats.html(output, {
+    theme: './node_modules/documentation-theme-light'
   })
 }
 
 /**
- * Get section for documentation
+ * @param {file[]} output
+ */
+async function saveDocs (output) {
+  await mkdir(API_FOLDER, { recursive: true })
+
+  output.forEach(async file => {
+    let fileName = file.history[0].replace(file.base, '')
+    let fileExt = path.extname(fileName)
+    let filePath = path.join(API_FOLDER, fileName)
+
+    if (fileExt) {
+      let content = file.contents
+
+      if (fileName === 'index.html') {
+        content = content
+          .toString()
+          .replace(
+            /regular blockmt1 quiet rounded/g,
+            'blockmt1 quiet rounded bold block h4 mt2'
+          )
+      }
+
+      await writeFile(filePath, content)
+    } else {
+      await mkdir(filePath, { recursive: true })
+    }
+  })
+}
+
+/**
  * @param {String} name Name section
  * @return {Object}
  */
@@ -177,56 +157,11 @@ function getSectionsSeparator (name = '') {
 }
 
 /**
- * Returns an array of values of the enumerable properties of an object
- * @param {Object} obj Object that contains the properties and methods
- * @return {Array}
+ * @typedef {Object} file
+ * @property {Object} stat
+ * @property {Buffer} contents
+ * @property {String[]} history
+ * @property {String} cwd
+ * @property {String} base
+ * @property {Boolean} isVinyl
  */
-function objectValues (obj) {
-  if (typeof Object.values === 'function') {
-    return Object.values(obj)
-  }
-
-  return Reflect.ownKeys(obj)
-    .reduce((all, key) => all.concat(concatValue(obj, key)), [])
-}
-
-/**
- * Returns value of item
- * @param {Object} obj Object that contains the properties and methods
- * @param {String} key Key of object
- * @return {*}
- */
-function concatValue (obj, key) {
-  let isEnum = obj.propertyIsEnumerable(key)
-  return typeof key === 'string' && isEnum ? [obj[key]] : []
-}
-
-class StreamArray extends Readable {
-  /**
-   * Create a new instance of StreamArray
-   *
-   * @access private
-   * @param {Array} list
-   */
-  constructor (list) {
-    if (!Array.isArray(list)) {
-      throw new TypeError('First argument must be an Array')
-    }
-
-    super({ objectMode: true })
-
-    this._i = 0
-    this._l = list.length
-    this._list = list
-  }
-
-  /**
-   * Read the next item from the source Array and push into NodeJS stream
-
-   * @access protected
-   * @desc Read the next item from the source Array and push into NodeJS stream
-   */
-  _read () {
-    this.push(this._i < this._l ? this._list[this._i++] : null)
-  }
-}
