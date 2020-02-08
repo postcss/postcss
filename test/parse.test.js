acf7bc2348d540da27e6cd4c6fec2665b5ac3322
Andrey Sitnik <andrey@sitnik.ru>
2020-02-07 22:45:30 -0800

    Fix rebase problems

--- test/parse.test.js
+++ test/parse.test.js
@@ -11,12 +11,10 @@ it('works with file reads', () => {
 })
 
 cases.each((name, css, json) => {
-  if (name !== 'custom-properties.css') {
-    it('parses ' + name, () => {
-      let parsed = cases.jsonify(parse(css, { from: name }))
-      expect(parsed).toEqual(json)
-    })
-  }
+  it('parses ' + name, () => {
+    let parsed = cases.jsonify(parse(css, { from: name }))
+    expect(JSON.parse(parsed)).toEqual(JSON.parse(json))
+  })
 })
 
 it('parses UTF-8 BOM', () => {
