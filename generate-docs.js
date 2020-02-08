4e7d932aadcbcc2d52d6129c95a0c005480d94ee
Andrey Sitnik <andrey@sitnik.ru>
2020-02-07 22:45:30 -0800

    Clean up code

--- generate-docs.js
+++ generate-docs.js
@@ -1,58 +1,39 @@
-let fs = require('fs')
-let path = require('path')
-let { promisify } = require('util')
+let { join, extname } = require('path')
 let documentation = require('documentation')
+let { promisify } = require('util')
+let fs = require('fs')
 
 let mkdir = promisify(fs.mkdir)
 let writeFile = promisify(fs.writeFile)
 
-let API_FOLDER = path.join(process.cwd(), 'api')
+const API_FOLDER = join(__dirname, 'api')
 
-documentation
-  .build('./lib/*.js', {
-    shallow: true,
-    github: true
-  })
-  .then(generateDocs)
-  .then(renderTemplate)
-  .then(saveDocs)
-  .catch(error => {
-    process.stderr.write(error.stack + '\n')
-    process.exit(1)
-  })
-
-/**
- * @param {Object} data Entrance data
- * @return {Array}
- */
 function generateDocs (data) {
   let docs = Object.values(data)
-
   docs
-    .filter(item => item.augments.length > 0)
+    .filter(doc => doc.augments.length > 0)
     .forEach(doc => {
       doc.augments.forEach(augment => {
-        let essence = docs
-          .find(essenceItem => essenceItem.name === augment.name)
-
+        let essence = docs.find(i => i.name === augment.name)
         if (essence) {
-          let instanceEssence = essence.members.instance
-            .filter(item => item.memberof === essence.name)
+          let instance = essence.members.instance
+            .filter(i => i.memberof === essence.name)
 
-          doc.members.instance = []
-            .concat(doc.members.instance, instanceEssence)
-            .map(item => ({
-              ...item,
-              namespace: item.namespace.replace(essence.name, doc.name)
+          doc.members.instance = doc.members.instance
+            .concat(instance)
+            .map(i => ({
+              ...i,
+              namespace: i.namespace.replace(essence.name, doc.name)
             }))
-            .filter((item, index, list) => {
-              return item.memberof === essence.name ||
-                (
-                  item.memberof === doc.name &&
-                  !list.find(e => e.name === item.name &&
-                    e.memberof === essence.name
-                  )
-                )
+            .filter((i, index, list) => {
+              if (i.memberof === essence.name) {
+                return true
+              } else {
+                let same = list.find(e => {
+                  return e.name === i.name && e.memberof === essence.name
+                })
+                return i.memberof === doc.name && !same
+              }
             })
             .sort((a, b) => a.name > b.name ? 1 : -1)
         }
@@ -62,8 +43,7 @@ function generateDocs (data) {
   docs
     .sort((a, b) => a.name > b.name ? 1 : -1)
     .forEach(doc => {
-      doc.members.instance = doc.members.instance
-        .filter(item => item.name)
+      doc.members.instance = doc.members.instance.filter(i => i.name)
     })
 
   let classes = docs.filter(item => item.kind === 'class')
@@ -73,10 +53,8 @@ function generateDocs (data) {
   return [].concat(
     getSectionsSeparator('Classes'),
     classes,
-
     getSectionsSeparator('Namespaces'),
     namespace,
-
     getSectionsSeparator('Global'),
     typedef
   )
@@ -88,40 +66,25 @@ function renderTemplate (output) {
   })
 }
 
-/**
- * @param {file[]} output
- */
 async function saveDocs (output) {
   await mkdir(API_FOLDER, { recursive: true })
-
   output.forEach(async file => {
-    let fileName = file.history[0].replace(file.base, '')
-    let fileExt = path.extname(fileName)
-    let filePath = path.join(API_FOLDER, fileName)
-
-    if (fileExt) {
-      let content = file.contents
-
-      if (fileName === 'index.html') {
-        content = content
-          .toString()
-          .replace(
-            /regular blockmt1 quiet rounded/g,
-            'blockmt1 quiet rounded bold block h4 mt2'
-          )
+    let name = file.history[0].replace(file.base, '')
+    if (extname(name)) {
+      let content = file.contents.toString()
+      if (name === 'index.html') {
+        content = content.replace(
+          /regular blockmt1 quiet rounded/g,
+          'blockmt1 quiet rounded bold block h4 mt2'
+        )
       }
-
-      await writeFile(filePath, content)
+      await writeFile(join(API_FOLDER, name), content)
     } else {
-      await mkdir(filePath, { recursive: true })
+      await mkdir(join(API_FOLDER, name), { recursive: true })
     }
   })
 }
 
-/**
- * @param {String} name Name section
- * @return {Object}
- */
 function getSectionsSeparator (name = '') {
   return {
     name: name.toUpperCase(),
@@ -156,12 +119,12 @@ function getSectionsSeparator (name = '') {
   }
 }
 
-/**
- * @typedef {Object} file
- * @property {Object} stat
- * @property {Buffer} contents
- * @property {String[]} history
- * @property {String} cwd
- * @property {String} base
- * @property {Boolean} isVinyl
- */
+documentation
+  .build('./lib/*.js', { shallow: true, github: true })
+  .then(generateDocs)
+  .then(renderTemplate)
+  .then(saveDocs)
+  .catch(error => {
+    process.stderr.write(error.stack + '\n')
+    process.exit(1)
+  })
