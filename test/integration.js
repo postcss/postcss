1d00c6899f636c9889ef3fb161ab1d55491c0b22
Andrey Sitnik <andrey@sitnik.ru>
2020-03-28 21:13:05 -0400

    Use unreleased postcss-parser-test

--- test/integration.js
+++ test/integration.js
@@ -1,25 +1,14 @@
 #!/usr/bin/env node
 
-let ciJobNumber = require('ci-job-number')
-let chalk = require('chalk')
 let real = require('postcss-parser-tests/real')
 
 let Processor = require('../lib/processor')
 let postcss = require('../')
 let pkg = require('../package')
 
-if (ciJobNumber() === 1) {
-  let instance = new Processor()
-  if (pkg.version !== instance.version) {
-    throw new Error('Version in Processor is not equal to package.json')
-  }
-
-  real(error => {
-    if (error) {
-      process.stderr.write(chalk.red(error.toString()) + '\n')
-      process.exit(1)
-    }
-  }, css => {
-    return postcss.parse(css).toResult({ map: { annotation: false } })
-  })
+let instance = new Processor()
+if (pkg.version !== instance.version) {
+  throw new Error('Version in Processor is not equal to package.json')
 }
+
+real(css => postcss.parse(css).toResult({ map: { annotation: false } }))
