7ca198fdd420c72082038cde46bee054c238b262
Andrey Sitnik <andrey@sitnik.ru>
2020-10-26 05:36:40 -0400

    Clarify offset/column start value

--- lib/node.d.ts
+++ lib/node.d.ts
@@ -22,12 +22,12 @@ export type ChildProps =
 
 export interface Position {
   /**
-   * Source offset in file.
+   * Source offset in file. It starts from 0.
    */
   offset: number
 
   /**
-   * Source line in file.
+   * Source line in file. In contrast to `offset` it starts from 1.
    */
   column: number
 
