a9153caa2905861efc7e4a29e5fbc1f26e801bbf
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-12-08 18:13:28 +0100

    Test stringifing fromJSON result

--- test/fromJSON.test.ts
+++ test/fromJSON.test.ts
@@ -6,7 +6,7 @@ it('rehydrates a JSON AST', () => {
   let cssWithMap = postcss().process(
     '.foo { color: red; font-size: 12pt; } /* abc */ @media (width: 60em) { }',
     {
-      from: undefined,
+      from: 'x.css',
       map: {
         inline: true
       },
@@ -24,6 +24,17 @@ it('rehydrates a JSON AST', () => {
   rehydrated.nodes[0].remove()
 
   expect(rehydrated.nodes).toHaveLength(3)
+
+  expect(
+    postcss().process(rehydrated, {
+      from: undefined,
+      map: {
+        inline: true
+      },
+      stringifier: postcss.stringify
+    }).css
+  ).toBe(`/* abc */ @media (width: 60em) { }
+/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInguY3NzIiwiPG5vIHNvdXJjZT4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQXNDLFNDQXRDLENEQWdELHdCQ0FoRCIsImZpbGUiOiJ0by5jc3MiLCJzb3VyY2VzQ29udGVudCI6WyIuZm9vIHsgY29sb3I6IHJlZDsgZm9udC1zaXplOiAxMnB0OyB9IC8qIGFiYyAqLyBAbWVkaWEgKHdpZHRoOiA2MGVtKSB7IH0iLG51bGxdfQ== */`)
 })
 
 it('throws when rehydrating an invalid JSON AST', () => {
