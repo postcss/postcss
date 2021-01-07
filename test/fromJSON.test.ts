52bcbd4270706536285e8deaf4038fa775c95231
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2021-01-07 12:33:44 +0100

    Cleanup: Typescript

--- test/fromJSON.test.ts
+++ test/fromJSON.test.ts
@@ -1,6 +1,6 @@
 import v8 from 'v8'
 
-import postcss, { Root, Declaration, Input } from '../lib/postcss.js'
+import postcss, { Root, Declaration, Input, Rule } from '../lib/postcss.js'
 
 it('rehydrates a JSON AST', () => {
   let cssWithMap = postcss().process(
@@ -19,7 +19,7 @@ it('rehydrates a JSON AST', () => {
   let json = root.toJSON()
   let serialized = v8.serialize(json)
   let deserialized = v8.deserialize(serialized)
-  let rehydrated = postcss.fromJSON(deserialized) as Root
+  let rehydrated = postcss.fromJSON(deserialized as object) as Root
 
   rehydrated.nodes[0].remove()
 
@@ -40,14 +40,11 @@ it('rehydrates a JSON AST', () => {
 it('rehydrates an array of Nodes via JSON.stringify', () => {
   let root = postcss.parse('.cls { color: orange; }')
 
-  // @ts-ignore
-  let json = JSON.stringify(root.nodes[0].nodes)
-  let rehydrated = (postcss.fromJSON(
-    JSON.parse(json)
-  ) as unknown) as Declaration[]
+  let rule = root.first as Rule
+  let json = JSON.stringify(rule.nodes)
+  let rehydrated = postcss.fromJSON(JSON.parse(json) as object[])
   expect(rehydrated[0]).toBeInstanceOf(Declaration)
-  // @ts-ignore
-  expect(rehydrated[0].source.input).toBeInstanceOf(postcss.Input)
+  expect(rehydrated[0].source?.input).toBeInstanceOf(Input)
 })
 
 it('throws when rehydrating an invalid JSON AST', () => {
