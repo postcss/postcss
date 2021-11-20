a11803f9d02542cb0c2117286533f093897940d2
Andrey Sitnik <andrey@sitnik.ru>
2021-11-21 00:35:57 +0100

    Clean up code style

--- lib/stringifier.d.ts
+++ lib/stringifier.d.ts
@@ -8,50 +8,29 @@ import {
   Rule,
   AtRule,
   Container
-} from './postcss.js';
+} from './postcss.js'
 
 export class Stringifier {
-  public constructor(builder: Builder);
-
-  public stringify(node: AnyNode, semicolon?: boolean): void;
-
-  public document(node: Document): void;
-
-  public root(node: Root): void;
-
-  public comment(node: Comment): void;
-
-  public decl(node: Declaration, semicolon?: boolean): void;
-
-  public rule(node: Rule): void;
-
-  public atrule(node: AtRule, semicolon?: boolean): void;
-
-  public body(node: Container): void;
-
-  public block(node: AnyNode, start: string): void;
-
-  public raw(node: AnyNode, own: string|null, detect?: string): string;
-
-  public rawSemicolon(root: Root): boolean|undefined;
-
-  public rawEmptyBody(root: Root): string|undefined;
-
-  public rawIndent(root: Root): string|undefined;
-
-  public rawBeforeComment(root: Root, node: Comment): string|undefined;
-
-  public rawBeforeDecl(root: Root, node: Declaration): string|undefined;
-
-  public rawBeforeRule(root: Root): string|undefined;
-
-  public rawBeforeClose(root: Root): string|undefined;
-
-  public rawBeforeOpen(root: Root): string|undefined;
-
-  public rawColon(root: Root): string|undefined;
-
-  public beforeAfter(node: AnyNode, detect: 'before'|'after'): string;
-
-  public rawValue(node: AnyNode, prop: string): string;
+  constructor(builder: Builder)
+  stringify(node: AnyNode, semicolon?: boolean): void
+  document(node: Document): void
+  root(node: Root): void
+  comment(node: Comment): void
+  decl(node: Declaration, semicolon?: boolean): void
+  rule(node: Rule): void
+  atrule(node: AtRule, semicolon?: boolean): void
+  body(node: Container): void
+  block(node: AnyNode, start: string): void
+  raw(node: AnyNode, own: string | null, detect?: string): string
+  rawSemicolon(root: Root): boolean | undefined
+  rawEmptyBody(root: Root): string | undefined
+  rawIndent(root: Root): string | undefined
+  rawBeforeComment(root: Root, node: Comment): string | undefined
+  rawBeforeDecl(root: Root, node: Declaration): string | undefined
+  rawBeforeRule(root: Root): string | undefined
+  rawBeforeClose(root: Root): string | undefined
+  rawBeforeOpen(root: Root): string | undefined
+  rawColon(root: Root): string | undefined
+  beforeAfter(node: AnyNode, detect: 'before' | 'after'): string
+  rawValue(node: AnyNode, prop: string): string
 }
