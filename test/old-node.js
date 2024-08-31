36950b4ff95d4077c0875a34a9c06821586589a8
Andrey Sitnik <andrey@sitnik.ru>
2024-08-31 23:21:31 +0200

    Try to fix old Node.js tests

--- test/old-node.js
+++ test/old-node.js
@@ -1,2 +1,12 @@
 // eslint-disable-next-line
 globalThis = Function('return this')()
+
+let Module = require('module')
+let originalRequire = Module.prototype.require
+
+Module.prototype.require = function (request) {
+  if (request.startsWith('node:')) {
+    request = request.slice(5)
+  }
+  return originalRequire.call(this, request)
+}
