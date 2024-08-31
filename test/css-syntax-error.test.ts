d524e969635e654c5274e9e4f21f86e564b29eda
Stepan <stepan.patrakeev@guarda.co>
2024-08-31 12:32:18 +0300

    - cut minified css considering block

--- test/css-syntax-error.test.ts
+++ test/css-syntax-error.test.ts
@@ -195,7 +195,7 @@ test('cut minified css', () => {
   let css = '.a{position:absolute!important;clip:rect(1px,1px,1px,1px);}.b{border:0;background;text-decoration:none;font-size:100%;}.c{position:absolute!important;clip:rect(1px,1px,1px,1px);}.d{position:absolute!important;clip:rect(1px,1px,1px,1px);}'
   is(
     parseError(css).showSourceCode(false),
-    '> 1 | ,1px);}.b{border:0;background;text-decor\n' +
+    '> 1 | ,1px);}.b{border:0;background;text-decoration:none\n' +
     '    |                    ^'
   )
 })
@@ -204,7 +204,7 @@ test('correct mark position on cut minified css', () => {
   let css = '.a{background;position:absolute!important;clip:rect(1px,1px,1px,1px);}.b{border:0;text-decoration:none;font-size:100%;}.c{position:absolute!important;clip:rect(1px,1px,1px,1px);}.d{position:absolute!important;clip:rect(1px,1px,1px,1px);}'
   is(
     parseError(css).showSourceCode(false),
-    '> 1 | .a{background;position:a\n' +
+    '> 1 | .a{background;position:absolute!im\n' +
     '    |    ^'
   )
 })
@@ -227,7 +227,9 @@ test('highlight cut minified css with colors', () => {
     pico.yellow(';') +
     'background' +
     pico.yellow(';') + '' +
-    'text-decor' +
+    'text-decoration' +
+    pico.yellow(':') +
+    'none' +
     '\n ' +
     pico.gray('   | ') +
     '                   ' +
