52b5758011de80d4e55cde742cb286af4f08e5b2
Romain Menke <romainmenke@gmail.com>
2026-08-15 17:40:53 +0200

    Fix `'source' does not exist in type 'DeclarationProps'` error

--- lib/declaration.d.ts
+++ lib/declaration.d.ts
@@ -28,7 +28,7 @@ declare namespace Declaration {
     }
   }
 
-  export interface DeclarationProps {
+  export interface DeclarationProps extends NodeProps {
     /** Whether the declaration has an `!important` annotation. */
     important?: boolean
     /** Name of the declaration. */
